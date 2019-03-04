26aa39ef4e3379f589a39d94cf1957e89e5134bc
Chris Coughlan <chris@coughlan.io>
2019-03-04 02:01:16 +0000

    update examples

--- example/coverage.js
+++ example/coverage.js
@@ -1,9 +1,11 @@
 const assert = require('assert');
 const Citymapper = require('../src');
 
-const API_KEY = process.env.CITYMAPPER_API_KEY;
+const {
+  CITYMAPPER_API_KEY: apiKey,
+} = process.env;
 
-const citymapper = new Citymapper(API_KEY);
+const citymapper = new Citymapper(apiKey);
 
 const params = {
   points: [
